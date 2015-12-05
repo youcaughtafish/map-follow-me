0882af5052590b1801830878daa4364bff0b3dde
Dylan Moore <dzmoore@gmail.com>
2015-12-05 10:04:45 -0500

    added static html map file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var testPath = path.join(__dirname + '/../public/html/map.html');
+  console.log(testPath);
+  res.sendFile(testPath);
 });
 
 module.exports = router;
